1401ea6e728866d66e9f7eec94ceaffb12a88443
Natarajan <nrajpillai@gmail.com>
2018-03-20 16:21:14 -0400

    [Pop up route details, Axle, Payment pulldown, #hover and Recent Trips]

--- js/Queue.js
+++ js/Queue.js
@@ -1,2 +1,61 @@
 //code.stephenmorley.org
-function Queue(){var a=[],b=0;this.getLength=function(){return a.length-b};this.isEmpty=function(){return 0==a.length};this.enqueue=function(b){a.push(b)};this.dequeue=function(){if(0!=a.length){var c=a[b];2*++b>=a.length&&(a=a.slice(b),b=0);return c}};this.peek=function(){return 0<a.length?a[b]:void 0}};
+function Queue(){
+  var a=[],
+  b=0;
+    this.getLength=function(){
+    return a.length-b
+    };
+    this.isEmpty=function(){
+    return 0==a.length
+    };
+    this.enqueue=function(b)
+    {
+      a.push(b)
+    };
+    this.dequeue=function(){
+    if(0!=a.length){
+      var c=a[b];
+        2*++b>=a.length&&(a=a.slice(b),b=0);
+        return c
+        }
+    };
+    this.peek=function()
+    {
+      return 0<a.length?a[b]:void 0
+    }
+};
+
+/*
+// Creates the queue
+var Queue = function() {
+    this.storage = {};
+    this.lastIn = 0;
+    this.firstOut = 0;
+}
+
+// Adds a value to the end of the chain
+Queue.prototype.enqueue = function(value) {
+    // Check to see if value is defined
+    if (value) {
+        this.storage[this.lastIn] = value;
+        this.lastIn++;
+    }
+}
+
+// Removes a value from the beginning of the chain
+Queue.prototype.dequeue = function() {
+    // Check to see if queue is empty
+    if (this.lastIn - this.firstOut === 0) {
+        return undefined;
+    }
+
+    var result = this.storage[this.firstOut];
+    delete this.storage[this.firstOut];
+    this.firstOut++;
+    return result;
+}
+
+// Returns the length of the queue
+Queue.prototype.size = function() {
+    return this.lastIn - this.firstOut;
+}*/
---
+++ js/mapTollRateForPaymentType.js
@@ -0,0 +1,7 @@
+function mapTollRateForPaymentType(result, paymentType){
+  var amount = 0;
+  if(paymentType==='sp'){
+  //  result.get[0].tollAmounts.sp
+  }
+
+}
